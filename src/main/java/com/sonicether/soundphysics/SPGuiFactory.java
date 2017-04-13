7bd0d5018784b0a40e8be10bdc6fb86bc9da05cf
daipenger <fobbes@web.de>
2017-04-13 21:40:25 +0200

    Fix git lowercase bug, step 2

--- src/main/java/com/sonicether/soundphysics/SPGuiFactory.java
+++ src/main/java/com/sonicether/soundphysics/SPGuiFactory.java
@@ -6,7 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
 
-public class SPGuiFactoryTEMP implements IModGuiFactory {
+public class SPGuiFactory implements IModGuiFactory {
 
 	@Override
 	public void initialize(Minecraft minecraftInstance) {
