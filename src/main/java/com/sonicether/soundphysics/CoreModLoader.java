2b2053afe3be633163071e4485a7496f686e972d
J4D3N <jaden.buchan@hotmail.co.uk>
2017-06-18 15:08:45 +0100

    Update to 1.12

--- src/main/java/com/sonicether/soundphysics/CoreModLoader.java
+++ src/main/java/com/sonicether/soundphysics/CoreModLoader.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 
-@MCVersion(value = "1.11.2")
+@MCVersion(value = "1.12")
 public class CoreModLoader implements IFMLLoadingPlugin {
 
 	@Override
